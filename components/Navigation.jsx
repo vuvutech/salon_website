05d02a87a82a6792af5cb41962f4c0c123cac424
Vuvtech Github <vuvutech@gmail.com>
2024-03-01 00:16:04 +0000

    great edit

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -59,7 +59,6 @@ const Navigation = () => {
   return (
     <Navbar fluid>
       <Navbar.Brand href="/">
-        {/* <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" /> */}
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
           Elsie Hair Salon
         </span>
